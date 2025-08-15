6e0f96ad4753ccb8f316406db16cf9be9eee5c1b
xiangxiongfly <2278779027@qq.com>
2025-08-15 17:56:08 +0800

    update

---
+++ xiaotuxian/src/api/detail.js
@@ -0,0 +1,21 @@
+import request from "@/utils/http.js";
+
+export function getDetail(id) {
+  return request({
+    url: "goods",
+    params: {
+      id
+    }
+  });
+}
+
+export function getHotGoodsApi({id, type, limit = 3}) {
+  return request({
+    url: "/goods/hot",
+    params: {
+      id,
+      type,
+      limit
+    }
+  });
+}
--- xiaotuxian/src/router/index.js
+++ xiaotuxian/src/router/index.js
@@ -1,4 +1,5 @@
 import Category from "@/views/category/Category.vue";
+import GoodsDetail from "@/views/detail/GoodsDetail.vue";
 import Home from "@/views/home/Home.vue";
 import Layout from "@/views/layout/Layout.vue";
 import Login from "@/views/login/Login.vue";
@@ -16,6 +17,7 @@ const router = createRouter({
         {path: "home", component: Home},
         {path: "category/:id", component: Category},
         {path: "category/sub/:id", component: SubCategory},
+        {path: "detail/:id", component: GoodsDetail},
       ]
     },
     {
